a00d54f109ed6008c7f05d1fce8f8ee76bc5e3e0
Mthompson35 <mthompson@hitachicm.us>
2026-06-11 10:41:22 -0400

    Task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,19 +57,26 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    var author = req.params.author
-    var bookKeys = Object.keys(books)
-    console.log(req.params.author);
-    var list = [];
-    for(let i = 0; i < bookKeys.length; i++){
-    let currentBook = bookKeys[i];
-    console.log(books[bookKeys[i]].author);
-    if (books[currentBook].author.toLowerCase() === author.toLowerCase()) {
-        list.push(books[currentBook]);
-    }
-    }
-    return res.status(200).send(JSON.stringify(list, null, 4));
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const author = req.params.author;
+
+    const fetchBookByAuthor = await new Promise((resolve, reject) => {
+      const matchingBooks = Object.values(books).filter(
+        (book) => book.author === author
+      );
+      if (matchingBooks.length > 0) {
+        resolve(matchingBooks);
+      } else {
+        reject(new Error(`No books found for author: ${author}`));
+      }
+    });
+
+
+    res.status(200).send(JSON.stringify(fetchBookByAuthor, null, 4));
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 // Get all books based on title
